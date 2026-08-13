c3bffe5f16ba79639f7a633ff3289dbf85e14792
SHAREKSH <165549710+SHAREKSH@users.noreply.github.com>
2026-08-13 23:47:56 +0530

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        
+        // Approach with Time:O(n) & Space:O(n)
+        int[] Left_to_Right=new int[nums.length];
+        int[] Right_to_Left=new int[nums.length];
+
+        //Forward prefix product 
+        Left_to_Right[0]=nums[0];
+        for(int i=1;i<nums.length;i++) Left_to_Right[i]=Left_to_Right[i-1]*nums[i];
+        
+        //Backward suffix product
+        Right_to_Left[nums.length-1]=nums[nums.length-1];
+        for(int i=nums.length-2;i>0;i--) Right_to_Left[i]=Right_to_Left[i+1]*nums[i];
+        
+        int[] op=new int[nums.length];
+        op[0]=Right_to_Left[1];
+        op[nums.length-1]=Left_to_Right[nums.length-2];
+        for(int i=1;i<nums.length-1;i++) op[i]=Left_to_Right[i-1]*Right_to_Left[i+1];
+        return op;
+    }
+}  
