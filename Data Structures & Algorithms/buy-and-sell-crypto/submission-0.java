8dc16d040a392e0e5676ef7e5dc73ba270c0e502
SHAREKSH <165549710+SHAREKSH@users.noreply.github.com>
2026-08-23 20:18:38 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int i=0;
+        int j=1;
+        int res=0;
+
+        while(j<prices.length){
+            if(prices[j]-prices[i]>0){
+                res=Math.max(res,prices[j]-prices[i]);
+                j++;
+            }else{
+            i=j;
+            j++;
+            }
+        }
+        return res;
+        
+    }
+}
