36e6d145f28264929fd1e9ab3db6b16ca7dc8fb7
SHAREKSH <165549710+SHAREKSH@users.noreply.github.com>
2026-08-30 10:59:39 +0530

    Add: sqrtx - submission-5

---
+++ Data Structures & Algorithms/sqrtx/submission-5.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int mySqrt(int x) {
+
+        long l=0;
+        long r=x/2;
+
+        if(x<=3){
+        if(x==1||x==2||x==3) return 1;
+        else return 0;
+        }
+
+
+        while(l<=r){
+           long  mid = l+(r-l)/2;
+
+            if(mid==x/mid){
+                return (int)mid;
+            }else if(mid<x/mid){
+                l=mid+1;
+            }else{
+              r=mid-1;
+            }
+        }
+        return (int)r;
+        
+    }
+}
